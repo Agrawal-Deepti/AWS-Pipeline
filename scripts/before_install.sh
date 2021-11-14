b3cd846703b11458cbeafb5a255f3ef675994559
Agrawal-Deepti <78319350+Agrawal-Deepti@users.noreply.github.com>
2021-11-13 21:50:51 -0500

    fixing node version on aws

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,9 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16.8.0
+nvm use 16.8.0
+#nvm install node
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/aws-2048-pipeline"
