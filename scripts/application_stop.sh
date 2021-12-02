012f2b01afc8c563867930f8797c7bb305b6eb4c
Agrawal-Deepti <78319350+Agrawal-Deepti@users.noreply.github.com>
2021-12-02 00:26:25 -0500

    fixing deploy

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
 #echo "Stopping any existing node servers"
-if sudo pgrep node; then sudo pkill node; fi
+if sudo pgrep node; then sudo pkill node; > app.stop.out.log 2> app.stop.out.log < /dev/null & fi
 #exit 0
