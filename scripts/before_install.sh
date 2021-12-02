1e385b29c432b5469349e05c6e454a19dc9d56fe
Agrawal-Deepti <78319350+Agrawal-Deepti@users.noreply.github.com>
2021-12-01 23:12:31 -0500

    fixing deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.8.0
-nvm use 16.8.0
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash > app.before.out.log 2> app.before.out.log < /dev/null & 
+. ~/.nvm/nvm.sh > app.before.out.log 2> app.before.out.log < /dev/null & 
+nvm install 16.8.0 > app.before.out.log 2> app.before.out.log < /dev/null & 
+nvm use 16.8.0 > app.before.out.log 2> app.before.out.log < /dev/null & 
 #nvm install node
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/aws-2048-pipeline"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
-  rm -r /home/ec2-user/aws-2048-pipeline
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
@@ -19,9 +18,9 @@ fi
 
 #remove existing deployment 
 if [ -d /home/ec2-user/aws-2048-pipeline/node_modules ]; then
-  rm -r /home/ec2-user/aws-2048-pipeline/node_modules
+  rm -r /home/ec2-user/aws-2048-pipeline/node_modules > app.before.out.log 2> app.before.out.log < /dev/null & 
 fi    
 
 if [ -f /home/ec2-user/aws-2048-pipeline/package-lock.json ]; then
-  rm -r /home/ec2-user/aws-2048-pipeline/package-lock.json
+  rm -r /home/ec2-user/aws-2048-pipeline/package-lock.json > app.before.out.log 2> app.before.out.log < /dev/null & 
 fi
