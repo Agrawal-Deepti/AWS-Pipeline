27bb6b80e0c410e2048de7633b11f22c608ac86f
Agrawal-Deepti <78319350+Agrawal-Deepti@users.noreply.github.com>
2021-12-02 00:16:31 -0500

    fixing deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,9 +12,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-npm install react-scripts --save
+sudo npm install react-scripts --save
 
 #start our node app in the background
-npm start > app.out.log 2> app.err.log < /dev/null & 
+sudo npm start > app.out.log 2> app.err.log < /dev/null & 
 
 #end script
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
 #echo "Stopping any existing node servers"
-if pgrep node; then pkill node; fi
+if sudo pgrep node; then sudo pkill node; fi
 #exit 0
